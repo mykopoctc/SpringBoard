ecb8e7aa3e18d13fe8d331057743a84ae8183b8d
zep25dr <zep25dr@naver.com>
2019-01-27 12:52:58 +0900

    first commit

---
+++ src/main/java/kr/ac/kopo/ctc/spring/board/web/HelloController.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.ctc.spring.board.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value = "/hello")
+	public String hellSpringBoot(Model model) {
+		model.addAttribute("name", "홍길동");
+		return "hello";
+	}
+
+}
