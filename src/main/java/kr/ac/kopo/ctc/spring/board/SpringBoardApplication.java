71aacbea8bb1273281823ae8ed00d21849ce3b9c
zep25dr <zep25dr@naver.com>
2019-01-27 15:51:54 +0900

    mysql / jpa / hibernate 추가

--- src/main/java/kr/ac/kopo/ctc/spring/board/SpringBoardApplication.java
+++ src/main/java/kr/ac/kopo/ctc/spring/board/SpringBoardApplication.java
@@ -1,12 +1,10 @@
 package kr.ac.kopo.ctc.spring.board;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class SpringBoardApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<body>
+	<div>Hello World MVC!</div>
+	<div>
+		<a href="hello">hello</a><br> 
+		<a href="sample/list">sample/list</a><br>
+	</div>
+</body>
+</html>
